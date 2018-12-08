126c2a6dbf8b8a1373b17adf01c0a9931c124788
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-08 13:05:50 +0000

    Add solution to second part of day 8

--- day08-python/day08.py
+++ day08-python/day08.py
@@ -1,5 +1,3 @@
-import sys
-
 def metadata_sum(tree):
     def aux(tree, i):
         n_nodes = tree[i]
@@ -14,8 +12,28 @@ def metadata_sum(tree):
         return [n_i + n_metadata, metadata_sum]
     return aux(tree, 0)[1]
 
+def value_of_root(tree):
+    def aux(tree, i):
+        n_nodes = tree[i]
+        n_metadata = tree[i + 1]
+        child_values = [0]
+        n_i = i + 2
+        for j in range(0, n_nodes):
+            [n_i, c_value] = aux(tree, n_i)
+            child_values.append(c_value)
+        value = 0
+        for j in range(0, n_metadata):
+            c = tree[n_i + j]
+            if n_nodes == 0:
+                value += c
+            elif (c < len(child_values)):
+                value += child_values[c]
+        return [n_i + n_metadata, value]
+    return aux(tree, 0)[1]
+
 tree = []
 with open("08.input") as f:
     tree = [int(x) for line in f for x in line.split()]
 
 print("Part 1: " + str(metadata_sum(tree)))
+print("Part 2: " + str(value_of_root(tree)))
