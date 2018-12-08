1a37e561e88a9b961727037548638811dfd51e85
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-08 13:27:16 +0000

    Do a single tree pass

--- day08-python/day08.py
+++ day08-python/day08.py
@@ -1,39 +1,28 @@
-def metadata_sum(tree):
-    def aux(tree, i):
-        n_nodes = tree[i]
-        n_metadata = tree[i + 1]
-        metadata_sum = 0
-        n_i = i + 2
-        for _ in range(0, n_nodes):
-            [n_i, c_sum] = aux(tree, n_i)
-            metadata_sum += c_sum
-        for j in range(0, n_metadata):
-            metadata_sum += tree[n_i + j]
-        return [n_i + n_metadata, metadata_sum]
-    return aux(tree, 0)[1]
-
-def value_of_root(tree):
-    def aux(tree, i):
-        n_nodes = tree[i]
-        n_metadata = tree[i + 1]
-        child_values = [0]
-        n_i = i + 2
-        for _ in range(0, n_nodes):
-            [n_i, c_value] = aux(tree, n_i)
-            child_values.append(c_value)
-        value = 0
-        for j in range(0, n_metadata):
-            c = tree[n_i + j]
-            if n_nodes == 0:
-                value += c
-            elif c < len(child_values):
-                value += child_values[c]
-        return [n_i + n_metadata, value]
-    return aux(tree, 0)[1]
+def go(tree, i = 0):
+    n_nodes = tree[i]
+    n_metadata = tree[i + 1]
+    child_values = [0]
+    metadata_sum = 0
+    value = 0
+    n_i = i + 2
+    for _ in range(0, n_nodes):
+        [n_i, c_sum, c_value] = go(tree, n_i)
+        metadata_sum += c_sum
+        child_values.append(c_value)
+    for j in range(0, n_metadata):
+        c = tree[n_i + j]
+        metadata_sum += c
+        if c < len(child_values):
+            value += child_values[c]
+    if n_nodes == 0:
+        value = metadata_sum
+    return [n_i + n_metadata, metadata_sum, value]
 
 tree = []
 with open("08.input") as f:
     tree = [int(x) for line in f for x in line.split()]
 
-print("Part 1: " + str(metadata_sum(tree)))
-print("Part 2: " + str(value_of_root(tree)))
+[_, metadata_sum, root_value] = go(tree)
+
+print("Part 1: " + str(metadata_sum))
+print("Part 2: " + str(root_value))
