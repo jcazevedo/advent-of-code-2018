c486309bcc19c676fedcfdcf1f0e2b2319ed5b52
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-12-17 03:17:23 +0000

    Add solution to second part of day 16

--- day16-scala/Day16.scala
+++ day16-scala/Day16.scala
@@ -1,5 +1,6 @@
 object Day16 extends App {
   sealed trait OpCode {
+    def name: String
     def a: Int
     def b: Int
     def c: Int
@@ -16,70 +17,86 @@ object Day16 extends App {
   }
 
   case class addr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "addr"
     def apply(r: Vector[Int]) = r.updated(c, r(a) + r(b))
   }
 
   case class addi(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "addi"
     def apply(r: Vector[Int]) = r.updated(c, r(a) + b)
   }
 
   case class mulr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "mulr"
     def apply(r: Vector[Int]) = r.updated(c, r(a) * r(b))
   }
 
   case class muli(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "muli"
     def apply(r: Vector[Int]) = r.updated(c, r(a) * b)
   }
 
   case class banr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "banr"
     def apply(r: Vector[Int]) = r.updated(c, r(a) & r(b))
   }
 
   case class bani(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "bani"
     def apply(r: Vector[Int]) = r.updated(c, r(a) & b)
   }
 
   case class borr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "borr"
     def apply(r: Vector[Int]) = r.updated(c, r(a) | r(b))
   }
 
   case class bori(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "bori"
     def apply(r: Vector[Int]) = r.updated(c, r(a) | b)
   }
 
   case class setr(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "setr"
     def apply(r: Vector[Int]) = r.updated(c, r(a))
   }
 
   case class seti(a: Int, b: Int, c: Int) extends TwoRegistersOpCode {
+    def name = "seti"
     def apply(r: Vector[Int]) = r.updated(c, a)
   }
 
   case class gtir(a: Int, b: Int, c: Int) extends OpCode {
+    def name = "gtir"
     def valid = List(b, c).forall(x => x >= 0 && x <= 3)
     def apply(r: Vector[Int]) = r.updated(c, if (a > r(b)) 1 else 0)
   }
 
   case class gtri(a: Int, b: Int, c: Int) extends OpCode {
+    def name = "gtri"
     def valid = List(a, c).forall(x => x >= 0 && x <= 3)
     def apply(r: Vector[Int]) = r.updated(c, if (r(a) > b) 1 else 0)
   }
 
   case class gtrr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "gtrr"
     def apply(r: Vector[Int]) = r.updated(c, if (r(a) > r(b)) 1 else 0)
   }
 
   case class eqir(a: Int, b: Int, c: Int) extends OpCode {
+    def name = "eqir"
     def valid = List(b, c).forall(x => x >= 0 && x <= 3)
     def apply(r: Vector[Int]) = r.updated(c, if (a == r(b)) 1 else 0)
   }
 
   case class eqri(a: Int, b: Int, c: Int) extends OpCode {
+    def name = "eqri"
     def valid = List(a, c).forall(x => x >= 0 && x <= 3)
     def apply(r: Vector[Int]) = r.updated(c, if (r(a) == b) 1 else 0)
   }
 
   case class eqrr(a: Int, b: Int, c: Int) extends AllRegistersOpcode {
+    def name = "eqrr"
     def apply(r: Vector[Int]) = r.updated(c, if (r(a) == r(b)) 1 else 0)
   }
 
@@ -101,16 +118,53 @@ object Day16 extends App {
       eqir(a, b, c),
       eqri(a, b, c),
       eqrr(a, b, c))
+
+    def apply(name: String, a: Int, b: Int, c: Int): Option[OpCode] =
+      all(a, b, c).find(_.name == name)
   }
 
   val lines = io.Source.fromFile("16.input").getLines().toList
-  val examples = lines.sliding(4, 4).filter(_.head.startsWith("Before:")).count { ll =>
+  val examples = lines.sliding(4, 4).filter(_.head.startsWith("Before:")).map { ll =>
     val regBefore = ll(0).stripPrefix("Before: [").stripSuffix("]").split(", ").toVector.map(_.toInt)
     val ops = ll(1).split(" ").toVector.map(_.toInt)
     val regAfter = ll(2).stripPrefix("After:  [").stripSuffix("]").split(", ").toVector.map(_.toInt)
+    (regBefore, ops, regAfter)
+  }.toList
+  val examplesWithCandidates = examples.count { case (b, ops, a) =>
     val opCodes = OpCode.all(ops(1), ops(2), ops(3))
-    opCodes.count(oc => oc.valid && oc(regBefore) == regAfter) >= 3
+    opCodes.count(oc => oc.valid && oc(b) == a) >= 3
   }
 
-  println(s"Part 1: $examples")
+  println(s"Part 1: $examplesWithCandidates")
+
+  val program = lines.reverse.takeWhile(_.size > 0).reverse.map { ll =>
+    val t = ll.split(" ").map(_.toInt)
+    (t(0), t(1), t(2), t(3))
+  }
+  val tempMappings = examples.foldLeft(Map.empty[Int, Set[String]]) { case (m, (b, ops, a)) =>
+    val validOpcodes = OpCode.all(ops(1), ops(2), ops(3)).filter(oc => oc(b) == a).map(_.name).toSet
+    m.get(ops(0)) match {
+      case Some(opcodes) => m.updated(ops(0), opcodes & validOpcodes)
+      case None => m + (ops(0) -> validOpcodes)
+    }
+  }
+
+  def cleanup(mappings: Map[Int, Set[String]]): Map[Int, Set[String]] = {
+    val single: Set[String] = mappings.filter(_._2.size == 1).flatMap(_._2).toSet
+    val next = mappings.mapValues(v => if (v.size > 1) v -- single else v)
+    if (next == mappings)
+      mappings
+    else
+      cleanup(next)
+  }
+
+  val mappings = cleanup(tempMappings)
+  assert(mappings.size == 16 && mappings.values.forall(_.size == 1))
+
+  val opCodeMappings = mappings.mapValues(_.head)
+  val registers = program.foldLeft(Vector(0, 0, 0, 0)) { case (reg, (o, a, b, c)) =>
+    OpCode.apply(opCodeMappings(o), a, b, c).fold(reg)(_.apply(reg))
+  }
+
+  println(s"Part 2: ${registers(0)}")
 }
